d2bea7b32f0439c65bb46d6780e3d5a1767fe158
KAZAHv2 <yehor.kandrin@nure.ua>
2025-06-13 10:09:18 +0300

    Реалізувати сервіс для автентифікації

---
+++ src/api/http/apiService.js
@@ -0,0 +1,52 @@
+import axios from 'axios'
+
+const apiService = axios.create({
+    baseURL: 'https://localhost:3000/',
+  },
+)
+
+apiService.interceptors.request.use((config) => {
+  if (localStorage.getItem('token')) {
+    config.headers = {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    }
+  }
+  return config
+})
+
+
+const getData = async (url) => {
+  try {
+    const response = await apiService.get(url)
+    return response.data
+  } catch (e) {
+    return Promise.reject(e)
+  }
+}
+const postData = async (url, payload) => {
+  try {
+    const response = await apiService.post(url, { ...payload })
+    return response.data
+  } catch (e) {
+    return Promise.reject(e)
+  }
+}
+const deleteData = async (url) => {
+  try {
+    const response = await apiService.delete(url)
+    return response.data
+  } catch (e) {
+    return Promise.reject(e)
+  }
+}
+const editData = async (url, payload) => {
+  try {
+    const response = await apiService.put(url, { ...payload })
+    return response.data
+  } catch (e) {
+    return Promise.reject(e)
+  }
+}
+
+export { getData, postData, deleteData, editData, getFile, apiService }
+
---
+++ src/stores/user.js
@@ -0,0 +1,88 @@
+import { defineStore } from 'pinia'
+import router from '../router'
+import {apiService, postData} from "@/api/http/apiService";
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+import { getDatabase} from "firebase/database"
+import {sendPasswordResetEmail,getAuth,createUserWithEmailAndPassword, updateProfile, signInWithEmailAndPassword, signOut,updatePassword,updateEmail} from 'firebase/auth'
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBK-6H5yNztvGBnn1AT7yh13xxXs16bR-U",
+  authDomain: "diplom-5ae23.firebaseapp.com",
+  projectId: "diplom-5ae23",
+  storageBucket: "diplom-5ae23.appspot.com",
+  messagingSenderId: "209774935604",
+  appId: "1:209774935604:web:8fba49382a7a8d40499461",
+  measurementId: "G-X4BTVV1H8B",
+  databaseURL: "https://diplom-5ae23-default-rtdb.europe-west1.firebasedatabase.app",
+}
+
+const firebaseApp = initializeApp(firebaseConfig)
+
+export const db = getDatabase(firebaseApp)
+export const auth = getAuth(firebaseApp)
+
+
+
+export const useUserStore = defineStore({
+  id: 'user',
+
+  state: () => ({
+    token: localStorage.getItem('token'),
+    user: {},
+    tokenValid: false,
+  }),
+
+  getters: {
+    isUserAuth: state => !!state.token,
+    getUserData: state => state.user,
+    tokenIsValid: state => state.tokenValid,
+  },
+
+  actions: {
+    setToken(token) {
+      this.token = token
+      localStorage.setItem('token', token)
+    },
+    async checkToken(){
+      try{
+        auth.onIdTokenChanged(user => {
+          if (user) {
+            this.tokenValid=true
+          } else {
+            this.tokenValid = false
+          }
+        })
+      }
+      catch(err){
+        this.tokenValid = false
+      }
+    },
+
+    async fetchUserData() {
+      this.user = auth.currentUser
+
+    },
+
+    async signUp(payload) {
+      const response = await postData('auth/register', payload)
+      this.setToken(response.token)
+      router.replace('/')
+
+    },
+
+
+  },
+})
+
+
+// npm run dev
+// firebase init
